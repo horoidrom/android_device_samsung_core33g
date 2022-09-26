b2cc4898d6e2e06ef2f9bfdf985f6b466b76f7d4
horoid <59495159+horoidrom@users.noreply.github.com>
2022-09-26 09:06:10 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,11 +47,11 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 androidboot.selinux=permissive androidboot.hardware=sc8830
 BOARD_KERNEL_PAGESIZE := 2048
 #TARGET_KERNEL_CONFIG := cyanogen_core33g_defconfig
 #TARGET_KERNEL_SOURCE := kernel/samsung/core33g
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --androidboot.selinux=permissive --dt device/samsung/core33g/dt.img
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/core33g/dt.img
 TARGET_PREBUILT_KERNEL   := device/samsung/core33g/zImage
 TARGET_PREBUILT_DTB      := device/samsung/core33g/dt
 
